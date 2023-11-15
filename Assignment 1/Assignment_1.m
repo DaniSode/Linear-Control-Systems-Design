9bfc3ebe614fe015fdf34437d0a91db0809c6464
DaniSode <danisode@student.chalmers.se>
2023-11-15 13:07:47 +0100

    Update Assignment_1.m

--- Assignment 1/Assignment_1.m
+++ Assignment 1/Assignment_1.m
@@ -15,7 +15,7 @@ clear all; close all; clc
 %% Definitions
 
 % Definitions of variables as syms
-syms i_a v_a_2 R L K_E K_T omega_1 omega_2 omega_3 phi_1 phi_2 phi_3 J_1 J_2 D_1 D_2 B_f T_e_2 real
+syms i_a v_a R L K_E K_T omega_1 omega_2 omega_3 phi_1 phi_2 phi_3 J_1 J_2 D_1 D_2 B_f T_e real
 syms i_a_dot omega_1_dot omega_2_dot omega_3_dot phi_1_dot phi_2_dot phi_3_dot real
 T_a = K_T*i_a;
 e_a = K_E*omega_1;
@@ -23,22 +23,22 @@ e_a = K_E*omega_1;
 % Define state and input vector
 x = [i_a; phi_1; omega_1; phi_2; omega_2; phi_3];
 xp = [i_a_dot; phi_1_dot; omega_1_dot; phi_2_dot; omega_2_dot; phi_3_dot];
-u_2 = [v_a_2; T_e_2];
+u = [v_a; T_e];
 
 % Define system equations
-equations = [v_a_2 == R*i_a + L*i_a_dot + e_a;
+equations = [v_a == R*i_a + L*i_a_dot + e_a;
              J_1*omega_1_dot == T_a - D_1*(phi_1 - phi_2);
              J_2*omega_2_dot == D_1*(phi_1 - phi_2) - D_2*(phi_2 - phi_3);
              phi_1_dot == omega_1;
              phi_2_dot == omega_2;
-             phi_3_dot == (D_2*(phi_2 - phi_3) + T_e_2)/B_f]; 
+             phi_3_dot == (D_2*(phi_2 - phi_3) + T_e)/B_f]; 
 disp('Equations:')
 equations = lhs(equations) - rhs(equations)
 
 % Define A and B matrices for hole system
 disp('All considered:')
 Am = -jacobian(equations, xp) \ jacobian(equations, x)
-Bm = -jacobian(equations, xp) \ jacobian(equations, u_2)
+Bm = -jacobian(equations, xp) \ jacobian(equations, u)
 
 % Neglect term with L solve for i_a and insert for all other i_a's
 first_eq = subs(equations(1), L, 0);
@@ -48,27 +48,31 @@ equations_new = subs(equations(2:end), i_a, i_a_new);
 % Define new A and B matrices neglecting L term
 disp('Considering L = 0:')
 A = -jacobian(equations_new, xp(2:end)) \ jacobian(equations_new, x(2:end))
-B = -jacobian(equations_new, xp(2:end)) \ jacobian(equations_new, u_2)
+B = -jacobian(equations_new, xp(2:end)) \ jacobian(equations_new, u)
 
 % Define C and D matrices for first case
 disp('Case 1:')
 y_1 = [phi_2; omega_2];
 C_a = jacobian(x(2:end), y_1)'
-D_a = jacobian(u_2, y_1)
+D_a = jacobian(u, y_1)
 
 % Define C and D matrices for second case
 disp('Case 2:')
 y_2 = [i_a; phi_3_dot];
 C_b = [-jacobian(first_eq, y_2(1)) \ jacobian(first_eq, x(2:end));
        -jacobian(equations_new(5), y_2(2)) \ jacobian(equations_new(5), x(2:end))]
-D_b = [-jacobian(first_eq, y_2(1)) \ jacobian(first_eq, u_2);
-       -jacobian(equations_new(5), y_2(2)) \ jacobian(equations_new(5), u_2)]
+D_b = [-jacobian(first_eq, y_2(1)) \ jacobian(first_eq, u);
+       -jacobian(equations_new(5), y_2(2)) \ jacobian(equations_new(5), u)]
 
 % Simulation case
-C_sim = [jacobian(x(2:end), [omega_1, omega_2])';
+C_sim = [-jacobian(first_eq, i_a) \ jacobian(first_eq, x(2:end));
+         jacobian(x(2:end), [phi_1, phi_2, phi_3])';
+         jacobian(x(2:end), [omega_1, omega_2])';
          -jacobian(equations_new(5), phi_3_dot) \ jacobian(equations_new(5), x(2:end))];
-D_sim = [jacobian(u_2, y_1);
-         -jacobian(equations_new(5), phi_3_dot) \ jacobian(equations_new(5), u_2)];
+D_sim = [-jacobian(first_eq, i_a) \ jacobian(first_eq, u);
+         jacobian(u, [phi_1, phi_2, phi_3])';
+         jacobian(u, [omega_1, omega_2])';
+         -jacobian(equations_new(5), phi_3_dot) \ jacobian(equations_new(5), u)];
 
 %% Substitution
 variables = [R; K_E; K_T; J_1; J_2; B_f; D_1; D_2];
@@ -117,59 +121,48 @@ trans_zeros = tzero(ss_model_2)
 %% Simulatation
 % Creating model and testing to simulate when ramping input
 ss_model_sim = ss(A_new, B_new, C_new_sim, D_new_sim);
-t_1 = 0:0.001:0.5;
-T_e_1 = min(0,max(-7*(t_1-0.25),-0.1));
-v_a_1 = max(0,min(600*(t_1-0.025),10));
-u_1 = [v_a_1; T_e_1];
-[y_2, t_sim_2] = lsim(ss_model_sim, u_1, t_1);
-
-% Simulating when stepwise changing input
-t_2 = linspace(0, 0.05, 1001);
-v_a_2 = [0*ones(1, (length(t_2)-1)*0.05), 10*ones(1, length(t_2)-(length(t_2)-1)*0.05)];
-T_e_2 = [0*ones(1, (length(t_2)-1)*0.5), -0.1*ones(1, length(t_2)-(length(t_2)-1)*0.5)];
-u_2 = [v_a_2; T_e_2];
-[y_1, t_sim_1] = lsim(ss_model_sim, u_2, t_2);
+t = 0:0.0001:0.05;
+T_e_sim = max(0,min(0.1/0.0001*(t-0.025),0.1));
+v_a_sim = max(0,min(10/0.0001*(t-0.0025),10));
+u_sim = [v_a_sim; T_e_sim];
+[y, t_sim] = lsim(ss_model_sim, u_sim, t);
 
 % Plotting
 figure
-subplot(4,3,1)
-plot(t_sim_2, y_2(:, 1))
+subplot(3,3,1)
+plot(t_sim, y(:, 2))
 axis padded; grid on
-title('Omega 1 (ramped input)'); xlabel('Time [s]'); ylabel('rad/s')
-subplot(4,3,2)
-plot(t_sim_2, y_2(:, 2))
+title('Phi 1'); xlabel('Time [s]'); ylabel('rad')
+subplot(3,3,2)
+plot(t_sim, y(:, 3))
 axis padded; grid on
-title('Omega 2 (ramped input)'); xlabel('Time [s]'); ylabel('rad/s')
-subplot(4,3,3)
-plot(t_sim_2, y_2(:, 3))
+title('Phi 2'); xlabel('Time [s]'); ylabel('rad')
+subplot(3,3,3)
+plot(t_sim, y(:, 4))
 axis padded; grid on
-title('Omega 3 (ramped input)'); xlabel('Time [s]'); ylabel('rad/s')
-subplot(4,3,7)
-plot(t_sim_1, y_1(:, 1))
+title('Phi 3'); xlabel('Time [s]'); ylabel('rad')
+subplot(3,3,4)
+plot(t_sim, y(:, 5))
 axis padded; grid on
-title('Omega 1 (step input)'); xlabel('Time [s]'); ylabel('rad/s')
-subplot(4,3,8)
-plot(t_sim_1, y_1(:, 2))
+title('Omega 1'); xlabel('Time [s]'); ylabel('rad/s')
+subplot(3,3,5)
+plot(t_sim, y(:, 6))
 axis padded; grid on
-title('Omega 2 (step input)'); xlabel('Time [s]'); ylabel('rad/s')
-subplot(4,3,9)
-plot(t_sim_1, y_1(:, 3))
+title('Omega 2'); xlabel('Time [s]'); ylabel('rad/s')
+subplot(3,3,6)
+plot(t_sim, y(:, 7))
 axis padded; grid on
-title('Omega 3 (step input)'); xlabel('Time [s]'); ylabel('rad/s')
-subplot(4,2,3)
-plot(t_1, u_1(1, :))
+title('Omega 3'); xlabel('Time [s]'); ylabel('rad/s')
+subplot(3,3,7)
+plot(t_sim, y(:, 1))
 axis padded; grid on
-title('Input voltage'); xlabel('Time [s]'); ylabel('v')
-subplot(4,2,4)
-plot(t_1, u_1(2, :))
-axis padded; grid on
-title('External forces'); xlabel('Time [s]'); ylabel('Nm')
-subplot(4,2,7)
-plot(t_2, u_2(1, :))
+title('Current'); xlabel('Time [s]'); ylabel('i')
+subplot(3,3,8)
+plot(t, u_sim(1, :))
 axis padded; grid on
 title('Input voltage'); xlabel('Time [s]'); ylabel('v')
-subplot(4,2,8)
-plot(t_2, u_2(2, :))
+subplot(3,3,9)
+plot(t, u_sim(2, :))
 axis padded; grid on
 title('External forces'); xlabel('Time [s]'); ylabel('Nm')
 
